f62547b6e916fc133a3dd33add76112123107ff0
Mike Stemle <mike.stemle@washpost.com>
2019-09-04 14:29:36 -0400

    Adding the `zeus_` prefix onto the slot IDs.

--- packages/ad/__tests__/index.test.js
+++ packages/ad/__tests__/index.test.js
@@ -5,8 +5,20 @@ import { ZeusAd } from "../lib";
 import renderer from "react-test-renderer";
 
 describe("@zeus-platform/ad", () => {
-  it("Should render and match snapshot", () => {
+  it("Should render and match snapshot with zeus_", () => {
+    const sample = renderer.create(<ZeusAd slotId="zeus_sample0" />);
+    expect(sample.toJSON().props.id).toEqual("zeus_sample0");
+    expect(sample.toJSON()).toMatchSnapshot();
+  });
+  it("Should render and match snapshot with zeus-", () => {
+    const sample = renderer.create(<ZeusAd slotId="zeus-sample0" />);
+    expect(sample.toJSON().props.id).toEqual("zeus_sample0");
+    expect(sample.toJSON()).toMatchSnapshot();
+  });
+
+  it("Should render and match snapshot without zeus_ or zeus-", () => {
     const sample = renderer.create(<ZeusAd slotId="sample0" />);
+    expect(sample.toJSON().props.id).toEqual("zeus_sample0");
     expect(sample.toJSON()).toMatchSnapshot();
   });
 });

c3a7d713f32009f04d13711998327f6850980469
Mike Stemle <mike.stemle@washpost.com>
2019-09-20 14:27:31 -0400

    Trying to add hooks to re-render upon navigation changes.

--- packages/ad/lib/index.js
+++ packages/ad/lib/index.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 /**
  * This component introduces a `zeus-ad` element to present an ad inventory slot.
  * @param {Object} props The properties to pass to the `zeus-ad` element.
  * @param {string} props.slotId The ID of the inventory slot.
  */
-export const ZeusAd = ({ slotId, ...props }) => {
+export const ZeusAd = ({ slotId, navigationPath, ...props }) => {
   const useSlotId = `zeus_${slotId.replace(/^zeus[_-]/, "")}`;
+
+  useEffect(() => {
+    const element = document.getElementById(useSlotId);
+    element && element.rerender();
+  });
+
   return <zeus-ad id={useSlotId} {...props} />;
 };
 
 ZeusAd.propTypes = {
-  slotId: PropTypes.string.isRequired
+  slotId: PropTypes.string.isRequired,
+  navigationPath: PropTypes.string
 };

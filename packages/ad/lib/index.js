c25cc71454c84bd19f0da32d3b4e9178bb523545
Mike Stemle <mike.stemle@washpost.com>
2019-09-23 12:31:55 -0400

    Adding changes for SPA refresh on navigation support.

--- packages/ad/lib/index.js
+++ packages/ad/lib/index.js
@@ -1,22 +1,25 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 /**
  * This component introduces a `zeus-ad` element to present an ad inventory slot.
  * @param {Object} props The properties to pass to the `zeus-ad` element.
  * @param {string} props.slotId The ID of the inventory slot.
  */
-export const ZeusAd = ({ slotId, navigationPath, ...props }) => {
-  const useSlotId = `zeus_${slotId.replace(/^zeus[_-]/, "")}`;
+export const ZeusAd = withRouter(
+  ({ slotId, changeOnNavigate = true, ...props }) => {
+    const useSlotId = `zeus_${slotId.replace(/^zeus[_-]/, "")}`;
 
-  useEffect(() => {
-    const element = document.getElementById(useSlotId);
-    element && element.rerender();
-  });
+    if (changeOnNavigate) {
+      const element = document.getElementById(useSlotId);
+      element && !!element.render && element.render();
+    }
 
-  return <zeus-ad id={useSlotId} {...props} />;
-};
+    return <zeus-ad id={useSlotId} {...props} />;
+  }
+);
 
 ZeusAd.propTypes = {
   slotId: PropTypes.string.isRequired,
-  navigationPath: PropTypes.string
+  changeOnNavigate: PropTypes.bool
 };

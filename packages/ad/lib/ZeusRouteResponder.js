1fc0816867630fd3b728fedb64396f97d1b225a1
Mike Stemle <mike.stemle@washpost.com>
2019-10-15 21:56:20 -0400

    Everything works, I need to test this tomorrow.

--- packages/ad/lib/ZeusRouteResponder.js
+++ packages/ad/lib/ZeusRouteResponder.js
@@ -15,8 +15,13 @@ const navigationFacts = { to: null, from: null, navigated: false };
 export const getNavigationFacts = () => navigationFacts;
 const setNavigationFacts = ({ from, to }) => {
   // If the routes are equal, we didn't navigate!
-  if (from === to || routesAreEqual(from, to)) {
-    return Object.assign(navigationFacts, { navigated: false });
+  if (from === to || !Object.keys(from).length || routesAreEqual(from, to)) {
+    return Object.assign(navigationFacts, {
+      navigated: false,
+      from,
+      to,
+      timestamp: new Date().getTime()
+    });
   }
 
   return Object.assign(navigationFacts, {
@@ -36,9 +41,10 @@ export let routeResponderReady = false;
 export const ZeusRouteResponderImpl = ({
   location,
   keyValuePairs = {},
+  debug = false,
   shouldChangeForRoute = () => true
 }) => {
-  const lastLocation = useRef(location);
+  const lastLocation = useRef({});
   const kvpRef = useRef(Object.assign({}, keyValuePairs));
 
   // Warn the user if they gave us an invalid property here.
@@ -48,21 +54,34 @@ export const ZeusRouteResponderImpl = ({
     );
     shouldChangeForRoute = () => true;
   }
-  setNavigationFacts({ from: lastLocation.current, to: location });
   useEffect(() => {
     kvpRef.current = Object.assign({}, keyValuePairs);
     // We need to know whether or not the route changed, and if the caller thinks
     // that it is a change worthy of an update.
-    const shouldTriggerChange = !routesAreEqual(lastLocation.current, location)
-      ? shouldChangeForRoute(location, lastLocation.current)
-      : false;
+    const shouldTriggerChange =
+      Object.keys(lastLocation.current).length &&
+      !routesAreEqual(lastLocation.current, location)
+        ? shouldChangeForRoute(location, lastLocation.current)
+        : false;
+
+    // Update the navigation facts
+    setNavigationFacts({ from: lastLocation.current, to: location });
+
+    // Debug
+    debug &&
+      console.log("ZEUS DEBUG: ZeusRouteResponder\nConsidering KVP update.", {
+        prev: lastLocation.current,
+        location,
+        navigationFacts: getNavigationFacts(),
+        shouldTriggerChange
+      });
     lastLocation.current = location;
 
     // Only change if we're supposed to change for this route.
     if (shouldTriggerChange) {
       triggerKeyValuePairsUpdate(kvpRef.current);
     }
-  }, [shouldChangeForRoute, location, keyValuePairs]);
+  }, [shouldChangeForRoute, location, keyValuePairs, debug]);
 
   routeResponderReady = true;
   return null;
--- packages/ad/lib/index.js
+++ packages/ad/lib/index.js
@@ -1,5 +1,20 @@
 import { ZeusAd } from "./ZeusAd";
-import { ZeusAdWithRouter } from "./ZeusAdWithRouter";
-import { ZeusRouteResponder } from "./ZeusRouteResponder";
+
+// All of this fun is to prevent folks without react-router-dom from going boom.
+let ZeusAdWithRouter = () => {
+  throw new Error("ZeusAdWithRouter did not load properly.");
+};
+let ZeusRouteResponder = () => {
+  throw new Error("ZeusRouteResponder did not load properly.");
+};
+
+try {
+  ZeusAdWithRouter = require("./ZeusAdWithRouter").ZeusAdWithRouter;
+  ZeusRouteResponder = require("./ZeusRouteResponder").ZeusRouteResponder;
+} catch (e) {
+  console.warn(
+    "You do not presently have react-router-dom installed. If you wish to use ZeusAdWithRouter and ZeusRouteResponder, you will need to install that module."
+  );
+}
 
 export { ZeusAd, ZeusAdWithRouter, ZeusRouteResponder };

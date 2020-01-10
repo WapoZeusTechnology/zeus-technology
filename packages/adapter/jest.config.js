ac9ffe65ffa91be0efce42015972c9f203f770b9
John Conway <john.conway@washpost.com>
2020-01-10 17:02:51 -0500

    Updated jest config

--- packages/adapter/jest.config.js
+++ packages/adapter/jest.config.js
@@ -1,14 +1,8 @@
 // jest.config.js
-const { defaults } = require("jest-config");
 module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  collectCoverageFrom: [
-    "**/*.{js,jsx}",
-    "!**/node_modules/**",
-    "!**/vendor/**",
-    "!**/coverage/**",
-    "!**/*.config.js"
-  ],
+  modulePathIgnorePatterns: ["/node_modules/", "/dist/"],
+  collectCoverageFrom: ["./lib/**/*.{js,jsx}"],
   collectCoverage: true,
   coverageDirectory: "./coverage"
 };

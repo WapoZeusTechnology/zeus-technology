dc3519761b874021ad955695abc85171d2eed6e2
John Conway <john.conway@washpost.com>
2020-01-16 10:04:21 -0500

    Allow private class members

--- packages/adapter/babel.config.js
+++ packages/adapter/babel.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
+    ["@babel/plugin-proposal-private-methods", { loose: false }],
     ["@babel/plugin-proposal-class-properties", { loose: false }],
     [
       "@babel/plugin-transform-runtime",

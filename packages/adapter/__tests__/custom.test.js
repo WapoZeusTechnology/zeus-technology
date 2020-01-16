cd3462d7ecf445d436e0f551e02a73c76059d68b
John Conway <john.conway@washpost.com>
2020-01-16 10:07:26 -0500

    Removed older tests

--- packages/adapter/__tests__/custom.test.js
+++
@@ -1,9 +0,0 @@
-// 'use strict';
-
-// const customSsp = require('..');
-
-describe("custom-ssp", () => {
-  it("needs tests", () => {
-    // First, let's try to verify that we can inject ourself into `zeus-lib`.
-  });
-});

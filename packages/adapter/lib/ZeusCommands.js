503f6e257a1f77b8d84271cebfac36e9b906eb0f
John Conway <john.conway@washpost.com>
2020-01-16 10:03:57 -0500

    Tested and finished loadScript function

--- packages/adapter/lib/ZeusCommands.js
+++ packages/adapter/lib/ZeusCommands.js
@@ -1,9 +1,14 @@
-const ScriptLoadWaitDelay = 20;
-const scriptDone = id => !!document.getElementById(id);
-const scriptDelay = () =>
-  new Promise(resolve => setTimeout(resolve, ScriptLoadWaitDelay));
+/**
+ * These are publically available commands that will be available on the ZeusAdapter. All of the
+ * commands in this list will be added to the ZeusAdapter object and available for users to call.
+ */
 
-let inBidding = false;
+import { delay, timeout } from "./utils";
+
+// let inBidding = false;
+const SCRIPT_LOAD_WAIT_DELAY = 20; // Time between checks for the script
+const SCRIPT_WAIT_TIMEOUT = 10000; // Time until bailing on waiting for the script to load
+const scriptDone = id => !!document.getElementById(id);
 
 export const ZeusCommands = {
   loadScript: async (scriptUrl, scriptId) => {
@@ -12,29 +17,24 @@ export const ZeusCommands = {
       scriptUrl
     });
 
-    // Sit in a wait loop until the script is ready.
-    // TODO: Should probably have a timeout here.
-    while (!scriptDone()) {
-      await scriptDelay();
-    }
+    // Waits until the script has been found on the page. Searches by scriptId for an element
+    // to be put on the dom.
+    const waitForScript = async () => {
+      // Sit in a wait loop until the script is ready.
+      while (!scriptDone(scriptId)) {
+        await delay(SCRIPT_LOAD_WAIT_DELAY);
+      }
+    };
 
-    return Promise.resolve();
-  },
-  biddingStarted: () => (inBidding = true),
-  biddingSuccess: () => {
-    inBidding &&
-      globalThis.zeus.emit("CUSTOM_BIDDING_FINISHED", { isSuccess: true });
-    inBidding = false;
-    return Promise.resolve();
-  },
-  biddingFail: (err = null) => {
-    inBidding &&
-      globalThis.zeus.emit("CUSTOM_BIDDING_FINISHED", {
-        isSuccess: false,
-        error: err
-      });
-    inBidding = false;
-    return Promise.resolve();
+    // Wait for script or cancel if a timeout occurs
+    return Promise.race([
+      waitForScript(),
+      timeout(
+        "Timeout waiting for script [some-script-id] to be added to dom.",
+        SCRIPT_WAIT_TIMEOUT
+      )
+    ]);
   }
 };
+
 Object.freeze(ZeusCommands);
